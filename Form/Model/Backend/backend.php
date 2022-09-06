621b6047a701477f9eda96cb4eab1bd3f518d767
AdedolapoAkanle <dolapoakanle@gmail.com>
2022-09-06 17:15:04 +0100

    new commit

--- Form/Model/Backend/backend.php
+++ Form/Model/Backend/backend.php
@@ -5,14 +5,14 @@ use function PHPSTORM_META\registerArgumentsSet;
 require('../../../Form/Model/Database/database.php');
 require("../../Controller/Classes/User/user.php");
 require("../../Controller/Classes/User/signin.php");
-require('../../../Form/Controller/Classes/parentRegister.php');
+require("../../Controller/Classes/parentRegister.php");
 require('../../Controller/CommonFunction/function.php');
 
 
-// if (isset($_POST['signup'])) {
-//     $signup = new Signup();
-//     $signup->processUser($_POST['name'], $_POST['email'], $_POST['password']);
-// }
+if (isset($_POST['register-btn'])) {
+    $register = new ParentRegister();
+    $register->processParent($_POST['title'], $_POST['full_name'], $_POST['main_phone'], $_POST['alt_phone'], $_POST['email'], $_POST['home_address']);
+}
 
 
 if (isset($_POST['signup'])) {
